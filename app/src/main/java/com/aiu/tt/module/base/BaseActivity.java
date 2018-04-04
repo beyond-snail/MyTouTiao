84886186394abd7e65279f243ef533430ac68f84
543701603@qq.com <wu15979937502>
2018-04-04 22:09:26 +0800

    提交一把

---
+++ app/src/main/java/com/aiu/tt/module/base/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.aiu.tt.module.base;
+
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2018/4/4 0004.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "BaseActivity";
+
+
+}
