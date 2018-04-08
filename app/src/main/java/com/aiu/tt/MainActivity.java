c5d510088d71ec091238c4d0c94276f94828c236
543701603@qq.com <wu15979937502>
2018-04-08 18:25:19 +0800

    提交一把

--- app/src/main/java/com/aiu/tt/MainActivity.java
+++ app/src/main/java/com/aiu/tt/MainActivity.java
@@ -3,11 +3,18 @@ package com.aiu.tt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.aiu.tt.module.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        
     }
 }
--- app/src/main/java/com/aiu/tt/module/base/BaseActivity.java
+++ app/src/main/java/com/aiu/tt/module/base/BaseActivity.java
@@ -1,7 +1,11 @@
 package com.aiu.tt.module.base;
 
+import android.content.Context;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.r0adkll.slidr.model.SlidrInterface;
 
 /**
  * Created by Administrator on 2018/4/4 0004.
@@ -9,6 +13,22 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
     private static final String TAG = "BaseActivity";
+    protected SlidrInterface slidrInterface;
+    protected Context mContext;
+    private int iconType = 1;
 
+
+    /**
+     * 初始化 ToolBar
+     * @param toolbar
+     * @param homeAsUpEnabled
+     * @param title
+     */
+    protected void initToolBar(Toolbar toolbar, boolean homeAsUpEnabled, String title){
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        //给左上角图标的左边加上一个返回的图标
+        getSupportActionBar().setDisplayHomeAsUpEnabled(homeAsUpEnabled);
+    }
 
 }
